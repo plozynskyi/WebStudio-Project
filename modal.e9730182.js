0ece8e1cb6e3be700f7a31fd3865b18ddd79742c
plozynskyi <93734829+plozynskyi@users.noreply.github.com>
2022-11-11 11:44:28 +0000

    Deploying to gh-pages from @ plozynskyi/WebStudio-Project@bb8db249863050ea1327860533b803044fe1e036 🚀

---
+++ modal.e9730182.js
@@ -0,0 +1,2 @@
+("undefined"!=typeof globalThis?globalThis:"undefined"!=typeof self?self:"undefined"!=typeof window?window:"undefined"!=typeof global?global:{}).parcelRequire42dc.register("5xtVg",(function(e,o){!function(){var e={openModalBtn:document.querySelector("[data-modal-open]"),closeModalBtn:document.querySelector("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}()}));
+//# sourceMappingURL=modal.e9730182.js.map
